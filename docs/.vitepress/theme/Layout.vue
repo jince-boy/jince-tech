0da36dda7cb18c084a81fff69cdc4b2286f1b599
Jin Ce <jince_hm@163.com>
2024-08-13 22:37:01 +0800

    feat: init project

---
+++ docs/.vitepress/theme/Layout.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+import DefaultTheme from "vitepress/theme";
+const { Layout } = DefaultTheme
+</script>
+
+<template>
+
+  <Layout>
+    <template #home-features>
+      My custom sidebar top content1222222222222222222222
+    </template>
+  </Layout>
+</template>
+
+<style scoped>
+
+</style>
