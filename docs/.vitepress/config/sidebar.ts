81644e2f97e95098a55c154089414573f16cef7f
JinCe <jince_hm@163.com>
2024-08-16 18:06:21 +0800

    feat: update SingletonPattern.md

--- docs/.vitepress/config/sidebar.ts
+++ docs/.vitepress/config/sidebar.ts
@@ -2,16 +2,10 @@ export const sidebar = {
     '/note/': [
         {text: '简介', link: '/note/'},
         {
-            text: 'Java',
+            text: '设计模式',
             collapsed: true,
             items: [
-                {
-                    text: '设计模式',
-                    collapsed: true,
-                    items: [
-                        {text: '单例模式', link: '/note/java/DesignPatterns/SingletonPattern'}
-                    ]
-                }
+                {text: '单例模式', link: '/note/DesignPatterns/SingletonPattern'}
             ]
         }
     ]
