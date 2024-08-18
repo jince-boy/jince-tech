a6d6417243bff2e836c21de45dd5b4e672a8ed92
Jin Ce <jince_hm@163.com>
2024-08-18 23:31:45 +0800

    feat: 编写工厂模式

--- docs/.vitepress/config/sidebar.ts
+++ docs/.vitepress/config/sidebar.ts
@@ -5,7 +5,9 @@ export const sidebar = {
             text: '设计模式',
             collapsed: true,
             items: [
-                {text: '单例模式', link: '/note/DesignPatterns/SingletonPattern'}
+                {text: '单例模式', link: '/note/DesignPatterns/SingletonPattern'},
+                {text: '工厂模式', link: '/note/DesignPatterns/FactoryPattern'},
+                {text: '策略模式', link: '/note/DesignPatterns/StrategyPattern'},
             ]
         }
     ]
